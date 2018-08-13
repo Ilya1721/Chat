c35b22cb354cef7dc8d282ed92c34a6d2141307b
Ilya1721 <ilyavusnya@gmail.com>
2018-08-13 13:27:46 +0300

    New version

--- ClChatClient/src/start/Client.java
+++ ClChatClient/src/start/Client.java
@@ -10,151 +10,294 @@ import javafx.scene.input.*;
 import javafx.event.*;
 import javafx.scene.control.*;
 import javafx.geometry.Pos;
+import javafx.concurrent.*;
 
 public class Client extends Application
 {
 	private static final int PORT = 7777;
-	private String host = "localhost";
+	private final String host = "localhost";
+	private final int width = 800;
+	private final int height = 600;
 	private Socket client;
-	private VBox main_layout;
-	private Label you;
-	private Label him;
-	private TextField message_out;
 	private Scene scene;
+	private VBox main_layout;
+	private HBox button_layout;
+	private HBox registrate_layout;
+	private HBox connect_layout;
+	private TextField outcoming_field;
+	private TextField name_field;
+	private TextArea incoming_field;
 	private Button close_button;
 	private Button send_button;
-	private HBox button_layout;
+	private Button registrate_button;
+	private Button connect_button;
+	private String user_name = "Please register your name";
+	private String line_from_server;
+	private String line_to_server;
+	private Thread connection_thread;
+	private Thread keep_thread;
+	private BufferedReader incoming;
+	private PrintWriter outcoming;
 	
 	public void start(Stage my_stage) 
 	{
-		init_ui();
-		open_connection();
-		hand_shake();
-		send_data();
-		get_data();
-		close_connection();
+		init_ui(width, height);
 		
-		my_stage.setScene(scene);
 		my_stage.setTitle("Client");
+		my_stage.setScene(scene);
 		my_stage.show();
+		my_stage.setOnCloseRequest(new EventHandler<WindowEvent>() 
+		{
+			public void handle(WindowEvent event) 
+			{
+				try 
+				{
+					System.out.println("Setting closing event");
+					if(client != null) 
+					{
+						client.close();
+					}
+					close_streams();
+					if(connection_thread.isAlive()) 
+					{
+						connection_thread.stop();
+					}
+					if(keep_thread.isAlive()) 
+					{
+						keep_thread.stop();
+					}
+				}
+				catch(Exception exc) 
+				{
+					System.out.println("Error at closing event");
+					System.out.println(exc.toString());
+				}
+			}
+		});
 	}
 	
 	public static void main(String args[]) 
 	{
 		launch(args);
 	}
-	
-	private void get_data() 
+
+	private void init_ui(int w, int h) 
 	{
-		while(true) 
+		System.out.println("Init ui");
+		main_layout = new VBox();
+		button_layout = new HBox();
+		registrate_layout = new HBox();
+		connect_layout = new HBox();
+		incoming_field = new TextArea();
+		outcoming_field = new TextField();
+		name_field = new TextField();
+		send_button = new Button("Send");
+		close_button = new Button("Close");
+		registrate_button = new Button("Registrate");
+		connect_button = new Button("Connect");
+		button_layout.getChildren().addAll(send_button, close_button);
+		registrate_layout.getChildren().addAll(registrate_button);
+		connect_layout.getChildren().addAll(connect_button);
+		main_layout.getChildren().addAll(incoming_field, outcoming_field, button_layout, 
+				name_field, registrate_layout, connect_layout);
+		button_layout.setAlignment(Pos.CENTER);
+		registrate_layout.setAlignment(Pos.CENTER);
+		connect_layout.setAlignment(Pos.CENTER);
+		scene = new Scene(main_layout, w, h);
+		set_buttons();
+	}
+	
+	private void set_buttons() 
+	{
+		try 
 		{
-			try 
+			System.out.println("Setting send button");
+			send_button.setOnAction(new EventHandler<ActionEvent>() 
 			{
-				System.out.println("Start getting data");
-				BufferedReader data_in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				System.out.println("Get Buffered Reader");
-				String line_from_server = data_in.readLine();
-				if(line_from_server == "") 
+				public void handle(ActionEvent event) 
 				{
-					System.out.println("Server has not sent data");
+					try 
+					{
+						System.out.println("Sending data from client to server");
+						line_to_server = outcoming_field.getText();
+						if(line_to_server != "") 
+						{
+							incoming_field.appendText(user_name + ": " + line_to_server + "\n");
+							outcoming.println(user_name + ": " + line_to_server);
+							outcoming.flush();
+						}
+					}
+					catch(Exception exc) 
+					{
+						System.out.println("Error at sending data to server");
+						System.out.println(exc.toString());
+					}
 				}
-				him.setText("Him: " + line_from_server);
-				System.out.println(line_from_server);
-				//data_in.close();
-			}
-			catch(Exception exc) 
+			});
+			System.out.println("Setting close button");
+			close_button.setOnAction(new EventHandler<ActionEvent>() 
 			{
-				System.out.println("Error at getting data from server");
-				System.out.println(exc.toString());
-			}
+				public void handle(ActionEvent event) 
+				{
+					try 
+					{
+						System.out.println("Closing connection with server");
+						if(client != null) 
+						{
+							client.close();
+						}
+					}
+					catch(Exception exc) 
+					{
+						System.out.println("Error at closing connection with server");
+						System.out.println(exc.toString());
+					}
+				}
+			});
+			System.out.println("Setting registrate button");
+			registrate_button.setOnAction(new EventHandler<ActionEvent>() 
+			{
+				public void handle(ActionEvent event) 
+				{
+					try 
+					{
+						System.out.println("Registrating name");
+						String name_field_text = name_field.getText();
+						if(name_field_text != "") 
+						{
+							user_name = name_field_text;
+						}
+					}
+					catch(Exception exc) 
+					{
+						System.out.println("Error at registrating name");
+						System.out.println(exc.toString());
+					}
+				}
+			}); 
+			System.out.println("Setting connect button");
+			connect_button.setOnAction(new EventHandler<ActionEvent>() 
+			{
+				public void handle(ActionEvent event) 
+				{
+					open_connection();
+				}
+			});
 		}
-	}
-	
-	private void send_data() 
-	{
-		send_button.setOnAction(new EventHandler<ActionEvent>() 
+		catch(Exception exc) 
 		{
-			public void handle(ActionEvent event)
-			{
-				try 
-				{
-					PrintWriter data_out = new PrintWriter(client.getOutputStream());
-					you.setText("You: " + message_out.getText());
-					data_out.println(message_out.getText());
-					data_out.flush();
-				}
-				catch(Exception exc) 
-				{
-					System.out.println("Error at sending data to server");
-					System.out.println(exc.toString());
-				}
-			}
-		});
-	}
-	
-	private void close_connection() 
-	{
-		close_button.setOnAction(new EventHandler<ActionEvent>() 
-		{
-			public void handle(ActionEvent event) 
-			{
-				try 
-				{
-					System.out.println("Closing connection");
-					client.close();
-				}
-				catch(Exception exc) 
-				{
-					System.out.println("Error at closing connection");
-					System.out.println(exc.toString());
-				}
-			}
-		});
+			System.out.println("Error at setting buttons");
+			System.out.println(exc.toString());
+		}
 	}
 	
 	private void open_connection() 
 	{
+		connection_thread = new Thread(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					System.out.println("Connecting to server");
+					client = new Socket(host, PORT);
+				}
+				catch(Exception exc) 
+				{
+					System.out.println("Error at connection to server");
+					System.out.println(exc.toString());
+				}
+				System.out.println("Connected to server");
+				set_streams();
+				keep_working();
+				while(true) 
+				{
+					get_data();
+				}
+			}
+		});
+		connection_thread.start();
+	}
+	
+	private void get_data() 
+	{
 		try 
 		{
-			System.out.println("Opening connection");
-			client = new Socket(host, PORT);
+			System.out.println("Getting data from server");
+			line_from_server = incoming.readLine();
+			if(!line_from_server.isEmpty()) 
+			{
+				incoming_field.appendText(line_from_server + "\n");
+			}
 		}
 		catch(Exception exc) 
 		{
-			System.out.println("Error at opening connection");
+			System.out.println("Error at getting data from server");
+			System.out.println(exc.toString());
+			connection_thread.stop();
+		}
+ 	}
+	
+	private void keep_working() 
+	{
+		keep_thread = new Thread(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					while(true) 
+					{
+						System.out.println("Sending empty string");
+						outcoming.println("");
+						outcoming.flush();
+						Thread.sleep(500);
+					}
+				}
+				catch(Exception exc) 
+				{
+					System.out.println("Error at keeping sending empty string");
+					System.out.println(exc.toString());
+					keep_thread.stop();
+				}
+			}
+		});
+		keep_thread.start();
+	}
+	
+	private void set_streams() 
+	{
+		try 
+		{
+			System.out.println("Setting streams with server");
+			incoming = new BufferedReader
+					(new InputStreamReader(client.getInputStream()));
+			outcoming = new PrintWriter(client.getOutputStream());
+		}
+		catch(Exception exc) 
+		{
+			System.out.println("Error at setting streams with server");
 			System.out.println(exc.toString());
 		}
 	}
 	
-	private void init_ui() 
-	{
-		main_layout = new VBox();
-		him = new Label("Him: ");
-		you = new Label("You: ");
-		message_out = new TextField();
-		close_button = new Button("Close");
-		send_button = new Button("Send");
-		button_layout = new HBox();
-		scene = new Scene(main_layout, 800, 600);
-		button_layout.getChildren().addAll(close_button, send_button);
-		button_layout.setAlignment(Pos.CENTER);
-		main_layout.getChildren().addAll(you, him, message_out, button_layout);	
-	}
-	
-	private void hand_shake() 
+	private void close_streams() 
 	{
 		try 
 		{
-			PrintWriter data_out = new PrintWriter(client.getOutputStream());
-			data_out.println("Connected");
-			data_out.flush();
+			System.out.println("Closing streams with server");
+			if(client != null) 
+			{
+				incoming.close();
+				outcoming.close();
+			}
 		}
 		catch(Exception exc) 
 		{
-			System.out.println("Error at handshaking");
+			System.out.println("Error at closing streams with server");
 			System.out.println(exc.toString());
 		}
-	}
+	}	
 }
 
 
--- ClChatServer/src/start/Server.java
+++ ClChatServer/src/start/Server.java
@@ -10,6 +10,7 @@ import javafx.scene.input.*;
 import javafx.event.*;
 import javafx.scene.control.*;
 import javafx.geometry.Pos;
+import javafx.concurrent.*;
 
 public class Server extends Application
 {	
@@ -17,22 +18,56 @@ public class Server extends Application
 	private Socket[] client;
 	private static final int PORT = 7777;
 	private static final int client_count = 2;
+	private final int width = 800;
+	private final int height = 600;
 	private String host = "localhost";
 	private VBox main_layout;
-	private HBox button_layout;
-	private Button close_button;
 	private Scene scene;
+	private Thread connection_thread;
+	private String line_from_client;
+	private BufferedReader incoming[];
+	private PrintWriter outcoming[];
 	
 	public void start(Stage my_stage) 
 	{
-		init_ui();
+		init_ui(width, height);
 		open_connection();
-		send_data();
-		close_connection();
 		
-		my_stage.setScene(scene);
 		my_stage.setTitle("Server");
+		my_stage.setScene(scene);
 		my_stage.show();
+		my_stage.setOnCloseRequest(new EventHandler<WindowEvent>() 
+		{
+			public void handle(WindowEvent event) 
+			{
+				try 
+				{
+					System.out.println("Setting closing event");
+					close_streams();
+					if(server != null) 
+					{
+						server.close();
+					}
+					if(client[0] != null) 
+					{
+						client[0].close();
+					}
+					if(client[1] != null) 
+					{
+						client[1].close();
+					}
+					if(connection_thread.isAlive()) 
+					{
+						connection_thread.stop();
+					}
+				}
+				catch(Exception exc) 
+				{
+					System.out.println("Error at closing event");
+					System.out.println(exc.toString()); 
+				}
+			}
+		});
 	}
 	
 	public static void main(String args[]) 
@@ -40,24 +75,74 @@ public class Server extends Application
 		launch(args);
 	}
 	
-	private void send_data() 
+	private void init_ui(int w, int h) 
+	{
+		System.out.println("Setting ui");
+		main_layout = new VBox();
+		scene = new Scene(main_layout, w, h);
+	}
+	
+	private void open_connection() 
+	{
+		connection_thread = new Thread(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					System.out.println("Opening server");
+					server = new ServerSocket(PORT);
+					client = new Socket[client_count];
+					for(int i = 0; i < client_count; ++i) 
+					{
+						System.out.println("Waiting for clients to connect");
+						client[i] = server.accept();
+					}
+				}
+				catch(Exception exc) 
+				{
+					System.out.println("Error at opening connection at server side");
+					System.out.println(exc.toString());
+				}
+				System.out.println("Clients connected");
+				set_streams();
+				while(true) 
+				{
+					get_data(0);
+					send_data(1);
+					get_data(1);
+					send_data(0);
+				}
+			}
+		});
+		connection_thread.start();
+	}
+	
+	private void get_data(int i) 
 	{
 		try 
 		{
-			System.out.println("Server sending data");
-			PrintWriter data_out = new PrintWriter(client[1].getOutputStream());
-			String server_string = get_data(client[0]);
-			System.out.println(server_string);
-			data_out.println(server_string);
-			data_out.flush();
-			//data_out.close();
-			data_out = new PrintWriter(client[0].getOutputStream());
-			server_string = get_data(client[1]);
-			System.out.println(server_string);
-			data_out.println(server_string);
-			data_out.flush();
-			//data_out.close();
-			System.out.println("Server has sent data");
+			System.out.println("Getting data from client");
+			line_from_client = incoming[i].readLine();	
+		}
+		catch(Exception exc) 
+		{
+			System.out.println("Error at getting data from client to server");
+			System.out.println(exc.toString());
+			connection_thread.stop();
+		}
+	}
+	
+	private void send_data(int i) 
+	{
+		try 
+		{
+			System.out.println("Sending data to client from server");
+			if(line_from_client != "")
+			{	
+				outcoming[i].println(line_from_client);
+				outcoming[i].flush();
+			}
 		}
 		catch(Exception exc) 
 		{
@@ -66,89 +151,52 @@ public class Server extends Application
 		}
 	}
 	
-	private void open_connection() 
+	private void set_streams() 
 	{
 		try 
 		{
-			System.out.println("Print test 1");
-			client = new Socket[client_count];
-			System.out.println("Print test 2");
-			server = new ServerSocket(PORT);
-			System.out.println("Print test 3");
+			incoming = new BufferedReader[client_count];
 			for(int i = 0; i < client_count; ++i) 
 			{
-				client[i] = server.accept();
-				System.out.println(client[i].getInetAddress().toString());
-				System.out.println("Get connection");
+				incoming[i] = new BufferedReader
+						(new InputStreamReader(client[i].getInputStream()));
 			}
-			System.out.println("Server got connection");
+			outcoming = new PrintWriter[client_count];
+			for(int i = 0; i < client_count; ++i) 
+			{
+				outcoming[i] = new PrintWriter(client[i].getOutputStream());
+			}
 		}
 		catch(Exception exc) 
 		{
-			System.out.println("Error at opening connection");
+			System.out.println("Error at setting streams with clients");
 			System.out.println(exc.toString());
 		}
 	}
 	
-	private void close_connection() 
-	{
-		close_button.setOnAction(new EventHandler<ActionEvent>() 
-		{
-			public void handle(ActionEvent event) 
-			{
-				try 
-				{
-					server.close();
-				}
-				catch(Exception exc) 
-				{
-					System.out.println("Error at closing connection");
-					System.out.println(exc.toString());
-				}
-			}
-		});
-	}
-	
-	private String get_data(Socket client) 
+	private void close_streams() 
 	{
 		try 
 		{
-			System.out.println("Server getting data");
-			System.out.println(client.getInetAddress().toString());
-			String message_in;
-			BufferedReader data_in;
-			data_in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			message_in = data_in.readLine();
-			//data_in.close();
-			if(message_in == "") 
+			for(int i = 0; i < client_count; ++i) 
 			{
-				message_in = "Null Error";
+				if(client[i] != null) 
+				{
+					outcoming[i].close();
+					incoming[i].close();
+				}
 			}
-			System.out.println("Server got data");
-			return message_in;
 		}
 		catch(Exception exc) 
 		{
-			String error = "Error at server getting data";
+			System.out.println("Error at closing streams with clients");
 			System.out.println(exc.toString());
-			return error; 
 		}
-	}
-	
-	private void init_ui() 
-	{
-		System.out.println("Init ui");
-		main_layout = new VBox();
-		button_layout = new HBox();
-		close_button = new Button("Close");
-		button_layout.getChildren().addAll(close_button);
-		button_layout.setAlignment(Pos.CENTER);
-		main_layout.getChildren().addAll(button_layout);
-		scene = new Scene(main_layout, 800, 600);
 	}
 }
 
-
+	
+	
 
 
 
